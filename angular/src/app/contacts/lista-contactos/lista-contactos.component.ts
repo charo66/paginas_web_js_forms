b4160c3d0af07a0dc7a4aa4dbc75c3e3920adae9
charo66 <chagarmol@gmail.com>
2018-10-04 14:01:01 +0200

    ANGULAR

--- angular/src/app/contacts/lista-contactos/lista-contactos.component.ts
+++ angular/src/app/contacts/lista-contactos/lista-contactos.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ContactoIf } from '../../models/contacto.model';
-import { USERS } from '../../datos/contactos.mock';
+import { UsuariosService } from '../../services/usuarios.service';
 
 @Component({
   selector: 'cas-lista-contactos',
@@ -10,12 +10,14 @@ import { USERS } from '../../datos/contactos.mock';
 export class ListaContactosComponent implements OnInit {
 
   aContactos: Array<ContactoIf>
-
-  constructor() { }
+  constructor(public usuarios: UsuariosService) { }
 
   ngOnInit() {
-    this.aContactos = USERS
-  }
+    this.aContactos = []
+  this.usuarios.getLista().then(
+    (response:any) => {this.aContactos=response}
+  )
+    }
 
   borrarContacto(badEmail) {
     
